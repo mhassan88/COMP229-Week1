65f6bd3d7209da3050e46e15ddbbbdd0972a4f66
mhassan88 <69553703+mhassan88@users.noreply.github.com>
2021-05-14 22:57:19 -0400

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+"use strict";
+var myVariable = 15;
+console.log("value of my variable = " + myVariable);
+
+for (let i = 1; i < 10; i++) {
+  console.log("value of i is " + i);
+}
+
+console.log(i);
